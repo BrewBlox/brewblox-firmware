f6c43d804a2e6208e156ba63f4955baea6108952
Elco Jacobs <elco@brewpi.com>
2019-04-24 16:47:47 +0200

    revert fixed point to string refactor, because it includes floating point math via CNL native_rounding

--- lib/src/FixedPoint.cpp
+++ lib/src/FixedPoint.cpp
@@ -24,7 +24,7 @@
 #include <stdlib.h>
 
 constexpr uint32_t
-pow10(int e)
+pow10(uint8_t e)
 {
     uint32_t r = 10;
     while (e--) {
@@ -37,29 +37,29 @@ pow10(int e)
 std::string
 to_string_dec(const fp12_t& t, uint8_t decimals)
 {
-    static constexpr fp12_t rounders[5] = {cnl::fraction<>(1, 2),
-                                           cnl::fraction<>(1, 20),
-                                           cnl::fraction<>(1, 200),
-                                           cnl::fraction<>(1, 2000),
-                                           cnl::fraction<>(1, 20000)};
 
-    if (decimals > 4) {
-        decimals = 4;
+    // shift to right number of digits
+    auto shifted = t * pow10(decimals);
+
+    // ensure correct rounding
+    auto rounder = fp12_t(0.5);
+    shifted = t >= fp12_t(0) ? shifted + rounder : shifted - rounder;
+
+    // convert to int
+    auto intValue = int32_t(shifted);
+
+    // convert to string
+    auto s = std::string();
+    s = s << intValue;
+
+    // prefix zeros for values smaller than 1
+    uint8_t insertPos = (intValue < 0) ? 1 : 0; // handle minus sign
+    uint8_t minChars = decimals + insertPos + 1;
+    if (minChars > s.size()) {
+        s.insert(insertPos, minChars - s.size(), '0');
     }
 
-    auto rounder = (t >= 0) ? rounders[decimals] : -rounders[decimals];
-    auto rounded = t + rounder;
-
-    auto digits = decimals + ((rounded < 0) ? 3 : 2);
-    auto temporary = abs(rounded);
-    while (temporary >= decltype(temporary)(10)) {
-        temporary = temporary / 10;
-        ++digits;
-    }
-
-    char buf[16] = {0};
-
-    cnl::to_chars(buf, buf + digits, rounded);
-
-    return std::string(buf);
+    // insert decimal point
+    s.insert(s.end() - decimals, '.');
+    return s;
 }
