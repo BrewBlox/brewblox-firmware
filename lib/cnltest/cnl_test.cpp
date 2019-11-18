6ea90cb5794d8605347e197692351d827a79e100
Elco Jacobs <elco@brewpi.com>
2019-11-18 17:09:42 +0100

    remove every x loops code

--- lib/cnltest/cnl_test.cpp
+++ lib/cnltest/cnl_test.cpp
@@ -24,7 +24,6 @@ clamp(const T& v, const T& lo, const T& hi)
 }
 
 const int max = 1000; // set to 100 when running with callgrind
-const int onlyTestEveryXLoops = 1;
 
 TEST_CASE("Fixedpoint calculations", "[cnltest]")
 {
@@ -43,13 +42,11 @@ TEST_CASE("Fixedpoint calculations", "[cnltest]")
 
             INFO(c); // this makes the tests slow!
 
-            if ((loop % onlyTestEveryXLoops) == 0) {
-                REQUIRE(c < decltype(c)(max * max));
-                REQUIRE(d >= 0);
-                if (c >= decltype(c)(4096)) {
-                    REQUIRE(cnl::unwrap(d) == (int64_t(1) << 23) - 1);
-                };
-            }
+            REQUIRE(c < decltype(c)(max * max));
+            REQUIRE(d >= 0);
+            if (c >= decltype(c)(4096)) {
+                REQUIRE(cnl::unwrap(d) == (int64_t(1) << 23) - 1);
+            };
         }
     }
 }
@@ -64,13 +61,14 @@ TEST_CASE("Equivalent integer calculations", "[cnltest]")
             loop++;
             auto c = (int64_t(a) * int64_t(b)) >> 12;
             int32_t d = clamp(c, -(int64_t(1) << 23), (int64_t(1) << 23) - 1);
-            if ((loop % onlyTestEveryXLoops) == 0) {
-                REQUIRE(c < int64_t(max * max) << 12);
-                REQUIRE(d >= 0);
-                if (c >= (int64_t(1) << 23) - 1) {
-                    REQUIRE(d == (int64_t(1) << 23) - 1);
-                };
-            }
+
+            INFO(c);
+
+            REQUIRE(c < int64_t(max * max) << 12);
+            REQUIRE(d >= 0);
+            if (c >= (int64_t(1) << 23) - 1) {
+                REQUIRE(d == (int64_t(1) << 23) - 1);
+            };
         }
     }
 }
