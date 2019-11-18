0e534b645ceaa46f45dc79bb864fcaadb8b0ae44
Elco Jacobs <elco@brewpi.com>
2019-11-18 15:53:44 +0100

    isolate cnl test in separate build

---
+++ lib/cnltest/cnl_test.cpp
@@ -0,0 +1,66 @@
+#define CATCH_CONFIG_MAIN
+#include <catch.hpp> // ensure that this is the single_include version
+
+#include "../cnl/include/cnl/num_traits.h"
+#include "../cnl/include/cnl/static_number.h"
+#include <catch.hpp>
+#include <cstdint>
+#include <limits>
+
+template <
+    int Digits,
+    int Exponent,
+    typename Narrowest = signed>
+using safe_elastic_fixed_point = cnl::static_number<Digits, Exponent, cnl::native_rounding_tag, cnl::saturated_overflow_tag, Narrowest>;
+
+using fp12_t = safe_elastic_fixed_point<23, -12>;
+
+template <class T>
+constexpr const T&
+clamp(const T& v, const T& lo, const T& hi)
+{
+    assert(!(hi < lo));
+    return (v < lo) ? lo : (hi < v) ? hi : v;
+}
+
+const int max = 1000; // set to 100 when running with callgrind
+
+TEST_CASE("Fixedpoint calculations", "[cnltest]")
+{
+
+    double aMax = max;
+    double bMax = max;
+    for (double a = 0; a < aMax; a += 1) {
+        for (double b = 0; b < bMax; b += 1) {
+            auto a_f = fp12_t(a);
+            auto b_f = fp12_t(b);
+
+            auto c = a_f * b_f;
+            auto d = fp12_t(c);
+
+            INFO(c);
+            INFO(d);
+
+            if (c >= decltype(c)(4096)) {
+                REQUIRE(cnl::unwrap(d) == (int64_t(1) << 23) - 1);
+            };
+        }
+    }
+}
+
+TEST_CASE("Equivalent integer calculations", "[cnltest]")
+{
+    int32_t aMax = max << 12;
+    int32_t bMax = max << 12;
+    for (int32_t a = 0; a < aMax; a += (1 << 12)) {
+        for (int32_t b = 0; b < bMax; b += (1 << 12)) {
+            auto c = (int64_t(a) * int64_t(b)) >> 12;
+            int32_t d = clamp(c, -(int64_t(1) << 23), (int64_t(1) << 23) - 1);
+            REQUIRE(c < int64_t(1000000) << 12);
+            REQUIRE(d >= 0);
+            if (c >= (int64_t(1) << 23) - 1) {
+                REQUIRE(d == (int64_t(1) << 23) - 1);
+            };
+        }
+    }
+}
