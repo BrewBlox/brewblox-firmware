e5f86e8d10188e90c05e8963d1542b4104e016fc
Bob Steers <steers.bob@gmail.com>
2020-03-26 12:57:51 +0100

    only check config file if not experimental

--- docker/enable-experimental.sh
+++ docker/enable-experimental.sh
@@ -10,27 +10,29 @@ SERVER_CFG="/etc/docker/daemon.json"
 # Ensure "jq" is installed.
 command -v jq > /dev/null || sudo apt-get install -y jq
 
-if [ ! -f "$CLIENT_CFG" ]; then
-    echo "Creating $CLIENT_CFG"
-    mkdir -p "$(dirname "$CLIENT_CFG")"
-    echo '{}' > "$CLIENT_CFG"
-fi;
-
 CLIENT_EXP=$(docker version --format '{{.Client.Experimental}}')
 if [[ "$CLIENT_EXP" != "true" ]]; then
+
+    if [ ! -f "$CLIENT_CFG" ]; then
+        echo "Creating $CLIENT_CFG"
+        mkdir -p "$(dirname "$CLIENT_CFG")"
+        echo '{}' > "$CLIENT_CFG"
+    fi;
+
     echo "Updating $CLIENT_CFG"
     OUTPUT=$(jq '. + {"experimental":"enabled"}' "$CLIENT_CFG")
     echo "$OUTPUT" > "$CLIENT_CFG"
 fi;
 
-if [ ! -f "$SERVER_CFG" ]; then
-    echo "Creating $SERVER_CFG"
-    sudo mkdir -p "$(dirname "$SERVER_CFG")"
-    echo '{}' | sudo tee "$SERVER_CFG"
-fi;
-
 SERVER_EXP=$(docker version --format '{{.Server.Experimental}}')
 if [[ "$SERVER_EXP" != "true" ]]; then
+
+    if [ ! -f "$SERVER_CFG" ]; then
+        echo "Creating $SERVER_CFG"
+        sudo mkdir -p "$(dirname "$SERVER_CFG")"
+        echo '{}' | sudo tee "$SERVER_CFG"
+    fi;
+
     echo "Updating $SERVER_CFG"
     OUTPUT=$(sudo jq '. + {"experimental":true}' "$SERVER_CFG")
     echo "$OUTPUT" | sudo tee "$SERVER_CFG"
